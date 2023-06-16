5c222ff46c9d9afc38a875ba8fd7c2229f2303ce
HaeJungg <gpwnd0120@gmail.com>
2023-06-16 17:56:36 +0900

    ADD: 초기 셋팅
    MODIFY: JWT install
    
    MODIFY: morgan supertest dependencie 이동
    
    MODIFY: 초기셋팅

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const { createApp } = require("./app.js");
+const appDataSource = require("./models/dataSource.js");
+
+const startServer = async () => {
+    const app = createApp();
+    const PORT = process.env.PORT || 3000;
+
+    appDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+      console.error("Error occurred during Data Source initialization", err);
+      appDataSource.destroy();
+    });
+
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+
+};
+
+startServer();
