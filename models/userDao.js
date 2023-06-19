833cb39597d211b40bb5bb149d12d51b88e8ddb0
Gaffeecate <unitnatis123@gmail.com>
2023-06-19 15:22:56 +0900

    ADD: 소셜 로그인 구현 완료
    로그인 임시
    
    REVISE:signin/up PR 수정사항
    
    Modify: field 명 수정  Login 구현완료
    
    외부 API Login

---
+++ models/userDao.js
@@ -0,0 +1,40 @@
+const appDataSource = require("./dataSource");
+
+const getUserById = async (kakaoId) => {
+  const users = await appDataSource.query(
+    `
+    SELECT
+        kakao_id,
+        email,
+        nickname,
+        gender,
+        age_range,
+        profile_image_url
+    FROM users
+    WHERE kakao_id=?`,
+    [kakaoId]
+  );
+  return users[0];
+};
+
+const signUp = async (email, name, kakaoId, gender, ageRange, profileImage) => {
+  await appDataSource.query(
+    `
+    INSERT INTO users(
+        email,
+        nickname,
+        kakao_id,
+        gender,
+        age_Range,
+        profile_image_url
+    ) VALUES (?, ?, ?, ?, ?, ?)`,
+    [email, name, kakaoId, gender, ageRange, profileImage]
+  );
+
+  return getUserById(kakaoId);
+};
+
+module.exports = {
+  getUserById,
+  signUp,
+};
---
+++ services/userService.js
@@ -0,0 +1,47 @@
+const { userDao } = require("../models");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+
+const signInKakao = async (kakaoToken) => {
+  const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
+    headers: {
+      Authorization: `Bearer ${kakaoToken}`,
+    },
+  });
+
+  if (result.status !== 200) {
+    throw new Error("CANNOT EXCESS TO KAKAOTOKEN");
+  }
+
+  const { data } = result;
+  const name = data.properties.nickname;
+  const email = data.kakao_account.email;
+  const kakaoId = data.id;
+  const gender = data.kakao_account.gender;
+  const ageRange = data.kakao_account.age_range;
+  const profileImage = data.kakao_account.thumbnail_image_url;
+
+  if (!name || !email || !kakaoId) throw new Error("KEY_ERROR", 400);
+
+  let user = await userDao.getUserById(kakaoId);
+
+  if (!user) {
+    await userDao.signUp(email, name, kakaoId, gender, ageRange, profileImage);
+    user = await userDao.getUserById(kakaoId);
+  }
+
+  const accessToken = jwt.sign(
+    { userId: user.userId },
+    process.env.JWT_SECRET,
+    {
+      algorithm: process.env.JWT_ALGORITHM,
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
+
+  return accessToken;
+};
+
+module.exports = {
+  signInKakao,
+};
