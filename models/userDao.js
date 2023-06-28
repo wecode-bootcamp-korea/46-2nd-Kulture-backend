0d14e4a28fc25f817b655d7a65c6a8a9c47ba2ce
HaeJungg <gpwnd0120@gmail.com>
2023-06-28 13:04:00 +0900

    🐣 ADD: user id 별 토큰 히스토리

--- models/userDao.js
+++ models/userDao.js
@@ -97,10 +97,49 @@ const updateUserInfo = async (nickname, profileImageUrl, userId) => {
   }
 }
 
+const tokenHistoryByUser = async(userId) => {
+  try {
+    const result = await appDataSource.query(`
+    SELECT subquery.user_id, subquery.token_type, subquery.event_token, subquery.name, subquery.date
+    FROM (
+      SELECT
+        user_id,
+        'charge' AS token_type,
+        amount AS event_token,
+        'Payment Charge' AS name,
+        DATE_FORMAT(created_at, '%Y-%m-%d') AS date
+      FROM event_token_histories
+      WHERE user_id = ?
+      UNION ALL
+      SELECT
+        o.user_id,
+        'usage' AS token_type,
+        o.total_event_token AS event_token,
+        e.name,
+        DATE_FORMAT(o.created_at, '%Y-%m-%d') AS date
+      FROM orders o
+      JOIN order_events oe ON o.id = oe.order_id
+      JOIN events e ON oe.event_id = e.id
+      WHERE o.user_id = ?
+    ) AS subquery
+    ORDER BY date DESC
+    `,
+    [userId, userId]
+    )
+    return result
+    } catch {
+      const error = new Error("dataSource Error");
+      error.statusCode = 400;
+
+      throw error;
+  }
+}
+
 module.exports = {
   getUserById,
   signUp,
   getUserByKakaoId,
   userInfo,
-  updateUserInfo
+  updateUserInfo,
+  tokenHistoryByUser,
 };
